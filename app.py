1df297dcdbca2eef71c1e6f6160f67ea54771eb5
Francis <bilodeaurobillardfrancis@gmail.com>
2020-05-28 13:09:47 -0400

    Feat: First configurations Config of the requirements, deployemnt and DB migrations

---
+++ app.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask
+from models import setup_db
+
+def create_app(test_config=None):
+    
+    app = Flask(__name__)
+    setup_db(app)
+    CORS(app)
+
+    @app.route('/')
+    def hello():
+        return "HELLO"
+
+app = create_app()
+
+if __name__ == "__main__":
+    app.run()
