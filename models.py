954a9d2181660ffe27439a7495ab5baba30b6f44
Francis <bilodeaurobillardfrancis@gmail.com>
2020-06-02 12:00:27 -0400

    Feat: migration init

--- models.py
+++ models.py
@@ -1,9 +1,15 @@
 import os
 from sqlalchemy import Column, String, create_engine
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import json
 
-database_path = os.environ['DB_URL']
+database_path = os.environ.get('DATABASE_URL')
+if not database_path:
+    database_name = "mitron"
+    database_pat = "postgres://{}@{}/{}".format('postgres: 61785',
+                                                'localhost: 5432',
+                                                 database_name)
 
 db = SQLAlchemy()
 
@@ -17,5 +23,30 @@ def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
-    db.create_all()
+    migrate = Migrate(app, db)
+    #db.create_all()
+
+class Category(db.Model):
+    __tablename__ = 'category'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    recipes =  db.relationship('Recipe', backref='category', lazy=True)
+
+class Recipe(db.Model):
+    __tablename__ = 'recipe'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    ingredients = db.relationship('Ingredient', backref='recipe', lazy=True)
+    time = db.Column(db.DateTime)
+    description = db.Column(db.String(500))
+    instruction = db.Column(db.String(5000))
+    
+class Ingredient(db.Model):
+    __tablename__ = 'ingredient'
+
+    id = Column(db.Integer, primary_key=True)
+    name = Column(db.String)
+
 
