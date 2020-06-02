c035766314a38b345ee1673c102459d252b827a5
Francis <bilodeaurobillardfrancis@gmail.com>
2020-06-02 13:02:32 -0400

    Feat: imported db functions

--- models.py
+++ models.py
@@ -1,15 +1,11 @@
 import os
-from sqlalchemy import Column, String, create_engine
+from sqlalchemy import Column, String, create_engine, Integer, DateTime
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 import json
 
 database_path = os.environ.get('DATABASE_URL')
 if not database_path:
-    database_name = "mitron"
-    database_pat = "postgres://{}@{}/{}".format('postgres: 61785',
-                                                'localhost: 5432',
-                                                 database_name)
+    database_path = "postgres://postgres:61785@localhost:5432/mitron"
 
 db = SQLAlchemy()
 
@@ -22,31 +18,29 @@ def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
-    db.init_app(app)
     migrate = Migrate(app, db)
-    #db.create_all()
+    db.init_app(app)
+
+
 
 class Category(db.Model):
     __tablename__ = 'category'
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
     recipes =  db.relationship('Recipe', backref='category', lazy=True)
 
 class Recipe(db.Model):
     __tablename__ = 'recipe'
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    ingredients = db.relationship('Ingredient', backref='recipe', lazy=True)
-    time = db.Column(db.DateTime)
-    description = db.Column(db.String(500))
-    instruction = db.Column(db.String(5000))
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    time = Column(DateTime)
+    description = Column(String(500))
+    instruction = Column(String(5000))
     
 class Ingredient(db.Model):
-    __tablename__ = 'ingredient'
+    __tablename__ = 'ingredients'
 
-    id = Column(db.Integer, primary_key=True)
-    name = Column(db.String)
-
-
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
